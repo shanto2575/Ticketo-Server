3f9c696559883bec3f6f6a25ce2b528c08953148
shanto2575 <shanto2575@gmail.com>
2026-06-10 19:00:51 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,100 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const dotenv = require('dotenv')
+dotenv.config()
+
+const uri = process.env.MONGODB_URI;
+
+const port = process.env.PORT;
+
+app.use(cors())
+app.use(express.json())
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+async function run() {
+    try {
+        // await client.connect();
+
+        const db = client.db('Ticketo')
+        const organizationCollection = db.collection('organization')
+        const eventsCollection = db.collection('events')
+        const bookingsCollection = db.collection('bookings')
+
+
+        app.get('/api/organization/:email', async (req, res) => {
+            const { email } = req.params;
+            const result = await organizationCollection.findOne({ organizerEmail: email })
+            res.json(result)
+            console.log(result)
+        })
+
+
+        app.post('/api/organization', async (req, res) => {
+            const { organizationName, logo, website, description, organizerEmail } = req.body;
+            const addData = {
+                organizationName,
+                logo,
+                website,
+                description,
+                organizerEmail,
+                createdAt: new Date(),
+                status: 'active'
+            }
+            const result = await organizationCollection.insertOne(addData)
+            // console.log(result,'re')
+            res.json(result)
+        })
+
+        app.patch('/api/organization/:id', async (req, res) => {
+            const {id}=req.params;
+            const { organizationName, logo, website, description, organizerEmail } = req.body;
+            const updateData = {
+                organizationName,
+                logo,
+                website,
+                description,
+                organizerEmail,
+                createdAt: new Date(),
+                status: 'active'
+            }
+            const result = await organizationCollection.updateOne(
+                { _id: new ObjectId(id) },
+                {
+                    $set: {
+                        ...updateData
+                    }
+                }
+            )
+            // console.log(result,'re')
+            res.json(result)
+        })
+
+
+
+
+
+
+        // await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
